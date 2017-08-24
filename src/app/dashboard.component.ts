b0a458bd08414f10c98ffb103a597714e39816d1
pranayp <pranayp@vmware.com>
2017-08-24 16:39:45 -0700

    text area applied

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './hero';
+import { Hero2 } from './hero2';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [
-    './dashboard.component.css'
-   ]
 })
 export class DashboardComponent {
-  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  powers = ['Node 6.11.1', 'Ruby 2.1.5', 'Go 1.3.3'];
 
+  model = new Hero2(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
+  submitted = false;
+
+  onSubmit() { this.submitted = true; console.log('Submitted', this.model); }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new Hero2(42, '', '');
+  }
 }
